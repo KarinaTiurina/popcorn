9fde996636c1f52044b6e0150181ea31081a992b
KarinaTiurina <rktiurina@gmail.com>
2017-09-29 15:52:34 +0300

    views/users update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def user_avatar(user)
+    asset_path('user.jpg')
+    # if user.avatar?
+    #   user.avatar.url
+    # else
+    #   asset_path('user.png')
+    # end
+  end
 end
