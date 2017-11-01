b4a63431972314417dee3253a58562bd02722407
KarinaTiurina <rktiurina@gmail.com>
2017-11-01 21:34:27 +0300

    comments views update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,20 +7,20 @@ class CommentsController < ApplicationController
     @new_comment.user = current_user
 
     if @new_comment.save
-      redirect_to @film, notice: 'Comment created'
+      redirect_to @film, notice: I18n.t('controllers.comments.created')
     else
-      render 'films/show', alert: 'Comment error'
+      render 'films/show', alert: I18n.t('controllers.comments.error')
     end
   end
 
 
   def destroy
-    message = {notice: 'Comment deleted'}
+    message = {notice: I18n.t('controllers.comments.destroyed')}
 
     if current_user_can_edit?(@comment)
       @comment.destroy!
     else
-      message = {alert: 'comment delete error'}
+      message = {alert: I18n.t('controllers.comments.error')}
     end
 
     redirect_to @film, message
